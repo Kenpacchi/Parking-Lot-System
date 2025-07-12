4ba575be258eefd14b2c4dff508c4f59ed0ad12d
Ajay <ajaygound.bhu@gmail.com>
2025-07-12 16:46:51 +0530

    First

---
+++ src/test/java/com/ParkingLotSystem/parkingSlotbooking/ParkingSlotbookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ParkingLotSystem.parkingSlotbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingSlotbookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
